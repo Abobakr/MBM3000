71a122cb8f60f41292237a7f2fbc006506c33a89
ABO BAKR ALMOSTAFA <abubaker_ms1@hotmail.com>
2025-01-11 01:12:54 +0300

    index page updated

---
+++ js/map-active.js
@@ -0,0 +1,32 @@
+let map;
+
+async function initMap() {
+  const position = { lat: 37.8731039, lng: 32.4954795 };
+
+  const { Map, InfoWindow } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  map = new Map(document.getElementById("map"), {
+    zoom: 20,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title:
+      "Our origins come from the Training & Development Center at 'Ebubekir for AI' Software Company. Officially registered in Turkiye @ 2019",
+    gmpClickable: true,
+  });
+
+  const infoWindow = new InfoWindow();
+
+  marker.addListener("click", ({ domEvent, latLng }) => {
+    const { target } = domEvent;
+    infoWindow.setContent(marker.title);
+    infoWindow.open(marker.map, marker);
+  });
+}
+
+initMap();
